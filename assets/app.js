f447a057529adea274b99a822ab25901e3a562d5
Jaoua OLLIVIER <jaoua.ollivier2025@campus-eni.fr>
2025-09-03 12:19:15 +0200

    #

--- assets/app.js
+++ assets/app.js
@@ -5,15 +5,6 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import { Turbo } from "@hotwired/turbo-rails";
-import { Application } from "@hotwired/stimulus";
-
-// Initialiser Turbo
-window.Turbo = Turbo;
-
-// Initialiser Stimulus
-window.Stimulus = Application.start();
-
 import './styles/app.css';
 
-console.log('App.js loaded: Stimulus + Turbo actifs!');
+console.log('App.js loaded!');
